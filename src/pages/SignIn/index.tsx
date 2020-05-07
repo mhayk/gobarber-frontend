a7db9d3595445a41803f7653d0d067fc7731d094
Mhayk Whandson <hi@mhayk.com>
2020-05-07 21:46:28 +0100

    The Input and Button have been isolated, transformed in a component.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
@@ -14,11 +17,10 @@ const SignIn: React.FC = () => (
       <form>
         <h1>Sign In</h1>
 
-        <input placeholder="E-mail" />
+        <Input name="email" icon={FiMail} placeholder="E-mail" />
+        <Input name="password" icon={FiLock} placeholder="Password" />
 
-        <input placeholder="Password" />
-
-        <button type="submit">Enter</button>
+        <Button type="submit">Enter</Button>
 
         <a href="forgot">I forgot my password</a>
       </form>
