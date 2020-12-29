5bb9af630eaca14f1f014462ed7ae5fc9d881b65
Mhayk Whandson <hi@mhayk.com>
2020-12-29 12:53:13 +0000

    The calendar has been added and styled.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
+import DayPicker, { DayModifiers } from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
 
 import { FiClock, FiPower } from 'react-icons/fi';
 import {
@@ -19,7 +21,13 @@ import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
   const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const handleDateChange = useCallback((day: Date, modifiers: DayModifiers) => {
+    if (modifiers.available) setSelectedDate(day);
+  }, []);
+
   const { signOut, user } = useAuth();
+
   return (
     <Container>
       <Header>
@@ -114,7 +122,33 @@ const Dashboard: React.FC = () => {
             </Appointment>
           </Section>
         </Schedule>
-        <Calendar />
+        <Calendar>
+          {/* weekdaysShort={['D', 'S', 'T', 'Q', 'Q', 'S', 'S']}
+          months={[
+            'Janeiro',
+            'Fevereiro',
+            'Março',
+            'Abril',
+            'Maio',
+            'Junho',
+            'Julho',
+            'Agosto',
+            'Setembro',
+            'Outubro',
+            'Novembro',
+            'Dezembro',
+          ]}
+          */}
+          <DayPicker
+            fromMonth={new Date()}
+            disabledDays={[{ daysOfWeek: [0, 6] }]}
+            modifiers={{
+              available: { daysOfWeek: [1, 2, 3, 4, 5] },
+            }}
+            selectedDays={selectedDate}
+            onDayClick={handleDateChange}
+          />
+        </Calendar>
       </Content>
     </Container>
   );
