cbe4b426595fb76ee1df71a4b72823e1d89f2920
Mhayk Whandson <hi@mhayk.com>
2020-12-29 11:45:30 +0000

    Appointment section has been created and styled.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { FiClock, FiPower } from 'react-icons/fi';
 import {
@@ -9,6 +9,8 @@ import {
   Content,
   Schedule,
   NextAppointment,
+  Section,
+  Appointment,
   Calendar,
 } from './styles';
 
@@ -16,6 +18,7 @@ import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
   const { signOut, user } = useAuth();
   return (
     <Container>
@@ -60,6 +63,56 @@ const Dashboard: React.FC = () => {
               </span>
             </div>
           </NextAppointment>
+
+          <Section>
+            <strong>Morning</strong>
+
+            <Appointment>
+              <span>
+                <FiClock /> 08:00
+              </span>
+              <div>
+                <img
+                  src="https://avatars2.githubusercontent.com/u/1500873?s=460&u=5a9ec92a1b593df93e818429effaf392df609119&v=4"
+                  alt=""
+                />
+
+                <strong>Mhayk Whandson</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock /> 08:00
+              </span>
+              <div>
+                <img
+                  src="https://avatars2.githubusercontent.com/u/1500873?s=460&u=5a9ec92a1b593df93e818429effaf392df609119&v=4"
+                  alt=""
+                />
+
+                <strong>Mhayk Whandson</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Afternoon</strong>
+
+            <Appointment>
+              <span>
+                <FiClock /> 08:00
+              </span>
+              <div>
+                <img
+                  src="https://avatars2.githubusercontent.com/u/1500873?s=460&u=5a9ec92a1b593df93e818429effaf392df609119&v=4"
+                  alt=""
+                />
+
+                <strong>Mhayk Whandson</strong>
+              </div>
+            </Appointment>
+          </Section>
         </Schedule>
         <Calendar />
       </Content>
